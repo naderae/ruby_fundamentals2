33e179955ef0b5012df87f8b22e5266fbfbae1bc
naderae <naderabouezze93@gmail.com>
2017-06-27 17:58:02 -0400

    1st

---
+++ exercise5.rb
@@ -0,0 +1,9 @@
+def greet_backwards(name)
+  name_in_reverse= name.reverse
+  puts "Hello #{name_in_reverse}#{name_in_reverse}, Welcome Home!"
+end
+
+greet_backwards('bob')
+greet_backwards('shirley')
+greet_backwards('sue')
+greet_backwards('andy')
