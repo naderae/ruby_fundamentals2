9086eb6b36cf46deee47061377d770273aac8c8e
naderae <naderabouezze93@gmail.com>
2017-06-27 23:23:25 -0400

    exercise 10 done

--- exercise10.rb
+++ exercise10.rb
@@ -14,6 +14,18 @@ end
 
 display(students)
 
-students[:cohort4] = 43
+students[:cohort4] = 43 #add an item to a hash
 
-puts students.keys 
+puts students.keys #display all the keys in the hash
+
+students_new = Hash[students.map do |x,y|[x, y * 1.05] end ]
+puts students_new
+# multiply all values in the hash by 1.05
+
+
+students_new.tap do |x|
+  x.delete (:cohort1)
+end
+puts students_new
+
+#deletes cohort1 from the hash.
